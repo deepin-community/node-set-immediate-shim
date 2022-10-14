e4c57b2261cef965b023cdc1825fa21ff3374971
Deepin Developer <deepin-dev@deepin.org>
2022-10-14 15:16:20 +0800

    feat: Init commit
    init node-set-immediate-shim
    
    Log:

---
+++ test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-global-assign, no-native-reassign */
+import test from 'ava';
+import importFresh from 'import-fresh';
+
+test.cb('shim', t => {
+	let called = false;
+
+	// Force the shim
+	const _ = setImmediate;
+	setImmediate = null;
+	const setImmediateShim = importFresh('.');
+	setImmediate = _;
+
+	setImmediateShim(() => {
+		called = true;
+		t.end();
+	});
+
+	t.false(called);
+});
+
+test.cb('pass rest arguments', t => {
+	// Force the shim
+	const _ = setImmediate;
+	setImmediate = null;
+	const setImmediateShim = importFresh('.');
+	setImmediate = _;
+
+	setImmediateShim((a, b) => {
+		t.is(a, 3);
+		t.is(b, 5);
+		t.end();
+	}, 3, 5);
+});
